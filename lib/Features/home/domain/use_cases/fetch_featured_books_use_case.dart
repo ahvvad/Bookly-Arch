2834c478f57f973d4a38e96dfabee1f7decdc82f
ahvvad <ahvadd.k@gmail.com>
2025-02-20 09:41:29 +0200

    create Fetch Featured Books Use Case

---
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+import '../entityes/book_entity.dart';
+import '../repos/home_repo.dart';
+
+class FetchFeaturedBooksUseCase {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase(this.homeRepo);
+
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
+    //check permission
+    return homeRepo.fetchFeaturedBooks();
+  }
+}
