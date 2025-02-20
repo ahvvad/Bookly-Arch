57c5bffbe2207b911d6544e4dda8a68c702a8c0d
ahvvad <ahvadd.k@gmail.com>
2025-02-20 17:40:23 +0200

    book model done

--- lib/Features/home/data/models/book_model/book_model.dart
+++ lib/Features/home/data/models/book_model/book_model.dart
@@ -1,9 +1,11 @@
+import 'package:arch_bookly/Features/home/domain/entityes/book_entity.dart';
+
 import 'access_info.dart';
 import 'sale_info.dart';
 import 'search_info.dart';
 import 'volume_info.dart';
 
-class BookModel {
+class BookModel extends BookEntity {
   String? kind;
   String? id;
   String? etag;
@@ -22,7 +24,14 @@ class BookModel {
     this.saleInfo,
     this.accessInfo,
     this.searchInfo,
-  });
+  }) : super(
+          bookId: id!,
+          image: volumeInfo?.imageLinks?.thumbnail ?? '',
+          authorName: volumeInfo?.authors?.first ?? 'No Author Found',
+          title: volumeInfo!.title!,
+          price: 0.0,
+          rating: volumeInfo.averageRating,
+        );
 
   factory BookModel.fromJson(Map<String, dynamic> json) => BookModel(
         kind: json['kind'] as String?,
