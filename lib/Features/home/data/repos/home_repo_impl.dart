8e9abf671c3adfcdfa07681c41ed082865e65f7c
ahvvad <ahvadd.k@gmail.com>
2025-02-24 10:02:33 +0200

    add cach & Network and finsh server failures

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -4,6 +4,7 @@ import 'package:arch_bookly/Features/home/domain/entityes/book_entity.dart';
 import 'package:arch_bookly/Features/home/domain/repos/home_repo.dart';
 import 'package:arch_bookly/core/errors/failure.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl extends HomeRepo {
   final HomeRemoteDataSource homeRemoteDataSource;
@@ -24,7 +25,10 @@ class HomeRepoImpl extends HomeRepo {
       books = await homeRemoteDataSource.fetchFeaturedBooks();
       return right(books);
     } catch (e) {
-      return left(Failure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
     }
   }
 
@@ -39,7 +43,10 @@ class HomeRepoImpl extends HomeRepo {
       books = await homeRemoteDataSource.fetchNewestBooks();
       return right(books);
     } catch (e) {
-      return left(Failure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
     }
   }
 }
--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,1 +1,53 @@
-class Failure {}
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String message;
+
+  Failure(this.message);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.message);
+  factory ServerFailure.fromDioException(DioException e) {
+    switch (e.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection Timeout with api server');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send Timeout with api server');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive Timeout with api server');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad Certificate with api server');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+          e.response!.statusCode!,
+          e.response!.data,
+        );
+      case DioExceptionType.cancel:
+        return ServerFailure('Request to api server was canceld');
+      case DioExceptionType.connectionError:
+        return ServerFailure('No Internet Connection');
+      case DioExceptionType.unknown:
+        return ServerFailure('Oops there was an Error, Please try again');
+    }
+  }
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 404) {
+      return ServerFailure('Your request was not found, please try later');
+    } else if (statusCode == 500) {
+      return ServerFailure('There is a problem with server, please try later');
+    } else if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else {
+      return ServerFailure('Oops there was an Error, Please try again');
+    }
+  }
+}
+
+class CacheFailure extends Failure {
+  CacheFailure(super.message);
+}
+
+class NetworkFailure extends Failure {
+  NetworkFailure(super.message);
+}
