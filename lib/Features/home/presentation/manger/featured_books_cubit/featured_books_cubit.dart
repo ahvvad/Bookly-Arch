ebaa9f89139aef272e18239a904bb5d9ff9763c9
ahvvad <ahvadd.k@gmail.com>
2025-02-25 23:31:16 +0200

    featured books cubit done

--- lib/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:arch_bookly/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
@@ -6,5 +7,20 @@ import '../../../domain/entityes/book_entity.dart';
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.fetchFeaturedBooksUseCase)
+      : super(FeaturedBooksInitial());
+
+  final FetchFeaturedBooksUseCase fetchFeaturedBooksUseCase;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await fetchFeaturedBooksUseCase.call();
+    result.fold(
+      (failure) => emit(
+        FeaturedBooksFailure(failure.message),
+      ),
+      (books) => emit(
+        FeaturedBooksSuccess(books),
+      ),
+    );
+  }
 }
